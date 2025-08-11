d1bde4eae470b380430ab6599ee48b34020fbd7a
MANR255926 <m.allyannadeem@gmail.com.com>
2025-08-11 14:59:58 +0500

    recursion

--- Notes/function.py
+++ Notes/function.py
@@ -6,3 +6,12 @@ n2=int(input('enter number: '))
 
 s = sum(n1,n2)
 print('your sum is: ', s)
+
+# function recursion/loop.
+def show(n):
+    if (n == 0): #stop condition.
+        return
+    print(n) 
+    show(n-1) #calling function in side the function to making it loop.
+    
+show(5) #calling a function out side the function for user & giving it value.
